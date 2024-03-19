fe9f6fb68b09c658f3ae2ffc84f875c37c3701c5
Patrik Johansson <patrik.johansson@if.se>
2024-03-19 12:34:21 +0100

    Added project for net8.0

---
+++ test/WebApi8-Swashbuckle/Models/RecordResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi8_Swashbuckle.Models;
+
+/// <summary>
+/// Record response.
+/// </summary>
+/// <remarks>
+/// Just record response.
+/// </remarks>
+/// <param name="Code">The code.</param>
+/// <param name="Name">The name.</param>
+public record RecordResponse(int Code, string Name);
